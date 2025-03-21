8604c836cb52b3a6a17da9bbac2f55f539b75c38
Arthur Henrique <103528304+arthurcst@users.noreply.github.com>
2025-03-21 03:48:28 -0300

    V0 finalizado

--- src/services/rooms/rooms-service.ts
+++ src/services/rooms/rooms-service.ts
@@ -1,123 +1,113 @@
+import { RiddleService } from "../riddles/riddle-service";
 import { User } from "./../user/user";
 import { Room } from "./room";
-import { SpotifyService } from "../spotify/spotify-service";
+// import { SpotifyService } from "../spotify/spotify-service";
 
-const spotifyService = new SpotifyService();
+// const spotifyService = new SpotifyService();
 
 export class RoomsService {
-  public Rooms: Room[] = [];
+    public Rooms: Room[] = [];
 
-  constructor() {}
+    constructor() {}
 
-  /**
-   * Creates a Room and add in Room list.
-   * @param steps Steps quantity
-   * @param owner Owner user instance
-   * @param name Room name (identifier)
-   * @param genre genre list
-   * @returns Room instance
-   */
-  public async createRoom(
-    steps: number,
-    owner: User,
-    name: string,
-    genre?: string[]
-  ) {
-    let newRoom = new Room(steps, owner, name, genre);
-    newRoom.trackList = await spotifyService.getRecommendations();
+    /**
+     * Creates a Room and add in Room list.
+     * @param steps Steps quantity
+     * @param owner Owner user instance
+     * @param name Room name (identifier)
+     * @param genre genre list
+     * @returns Room instance
+     */
+    public async createRoom(steps: number, owner: User, name: string) {
+        let newRoom = new Room(steps, owner, name);
+        let riddleService = new RiddleService();
 
-    newRoom.addPlayer(owner);
+        newRoom.riddles = riddleService.sortRiddles(steps);
 
-    this.addRoom(newRoom);
+        newRoom.addPlayer(owner);
 
-    return newRoom;
-  }
+        this.addRoom(newRoom);
 
-  /**
-   * Restart the specified room attributes
-   * @param roomName Room identifier
-   * @returns Room instance (reseted)
-   */
-  public async restartRoom(roomName: string) {
-    let room = this.Rooms.find((room) => room.name == roomName);
-
-    if (room) {
-      room.trackList = await spotifyService.getRecommendations();
-      room.started = false;
-      room.finished = false;
-      room.resetPlayers();
-
-      room.addPlayer(room.owner);
-      return room;
-    } else {
-      throw new Error("Room not found");
+        return newRoom;
     }
-  }
 
-  /**
-   * Add the new room into the rooms list
-   * @param room Room instance to be added on list
-   */
-  private addRoom(room: Room): void {
-    this.Rooms.push(room);
-  }
+    /**
+     * Restart the specified room attributes
+     * @param roomName Room identifier
+     * @returns Room instance (reseted)
+     */
+    public async restartRoom(roomName: string) {
+        let room = this.Rooms.find((room) => room.name == roomName);
 
-  /**
-   * Fetch especific room by identifier
-   * @param roomName find room by identifier
-   * @returns Room instance | undefined
-   */
-  public getRoom(roomName: string): Room | undefined {
-    return this.Rooms.find((room) => room.name === roomName);
-  }
+        if (room) {
+            // room.trackList = await spotifyService.getRecommendations();
+            room.started = false;
+            room.finished = false;
+            room.resetPlayers();
 
-  /**
-   * Fetch Rooms list
-   * @returns Room list
-   */
-  public getRooms(): Room[] {
-    return this.Rooms;
-  }
-
-  /**
-   * Find room by userName
-   * @param userName Username string
-   * @returns Room instance | undefined
-   */
-  public getRoomsByUserName(userName: string): Room | undefined {
-    return this.Rooms.find((room) => {
-      room.players.some((playerElement) => playerElement.name === userName);
-    });
-  }
-
-  /**
-   * Find room by userId
-   * @param userId user identifier string
-   * @returns Room instance | undefined if not found
-   */
-  public getRoomsByUserId(userId: string): Room | undefined {
-    const room = this.Rooms.find((room) => {
-      const hasPlayer = room.players.some(
-        (playerElement) => playerElement.socketId === userId
-      );
-      const hasOwner = room.owner.socketId === userId;
-      const functionReturn = hasPlayer || hasOwner;
-      return functionReturn;
-    });
-    if (room) {
-      return room;
-    } else {
-      return undefined;
+            room.addPlayer(room.owner);
+            return room;
+        } else {
+            throw new Error("Room not found");
+        }
     }
-  }
 
-  /**
-   * Fetch room by genre
-   * @deprecated
-   */
-  public getRoomsBygenre(genreTracked: string): Room[] {
-    return this.Rooms.filter((room) =>
-      room.genres.find((genre) => genre === genreTracked)
-    );
-  }
+    /**
+     * Add the new room into the rooms list
+     * @param room Room instance to be added on list
+     */
+    private addRoom(room: Room): void {
+        this.Rooms.push(room);
+    }
+
+    /**
+     * Fetch especific room by identifier
+     * @param roomName find room by identifier
+     * @returns Room instance | undefined
+     */
+    public getRoom(roomName: string): Room | undefined {
+        return this.Rooms.find((room) => room.name === roomName);
+    }
+
+    /**
+     * Fetch Rooms list
+     * @returns Room list
+     */
+    public getRooms(): Room[] {
+        return this.Rooms;
+    }
+
+    /**
+     * Find room by userName
+     * @param userName Username string
+     * @returns Room instance | undefined
+     */
+    public getRoomsByUserName(userName: string): Room | undefined {
+        return this.Rooms.find((room) => {
+            room.players.some(
+                (playerElement) => playerElement.name === userName
+            );
+        });
+    }
+
+    /**
+     * Find room by userId
+     * @param userId user identifier string
+     * @returns Room instance | undefined if not found
+     */
+    public getRoomsByUserId(userId: string): Room | undefined {
+        const room = this.Rooms.find((room) => {
+            const hasPlayer = room.players.some(
+                (playerElement) => playerElement.socketId === userId
+            );
+            const hasOwner = room.owner.socketId === userId;
+            const functionReturn = hasPlayer || hasOwner;
+            return functionReturn;
+        });
+        if (room) {
+            return room;
+        } else {
+            return undefined;
+        }
+    }
 }
--- src/services/spotify/spotify.ts
+++
@@ -1,8 +0,0 @@
-export interface Track {
-  name: string;
-  artist: string;
-  album: string;
-  album_art: string;
-  preview_url: string;
-  uri: string;
-}
